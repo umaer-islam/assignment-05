f0e241dcdcd7faab9778137ad183ec2ceae5f0c3
umaer-islam <umaerislam23@gmail.com>
2026-09-13 15:38:26 +0600

    feat: add Navbar component with desktop and mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Dev Stack</h1>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,30 @@
+import logo from "../assets/logo-text.png";
+
 function Navbar() {
   return (
-    <nav>
-      <h2>Navbar</h2>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <button className="menu-button">☰</button>
+
+        <img src={logo} alt="Dev Stack" />
+      </div>
+
+      <div className="navbar-links">
+        <a href="#">Home</a>
+        <a href="#">Technologies</a>
+        <a href="#">Projects</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+
+      <div className="navbar-actions">
+        <a href="#">Sign In</a>
+        <a href="#" className="signup-button">
+          Sign Up
+        </a>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
